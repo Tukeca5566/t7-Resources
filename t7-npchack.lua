624188df1e4d87cfb60fe349b1ffd21001e1df2d
tuke7a | GitHub <94323446+Tukeca5566@users.noreply.github.com>
2025-02-03 23:29:29 +0200

    Add files via upload

---
+++ t7-npchack.lua
@@ -0,0 +1,6 @@
+{
+    "version": "1.0.2",
+    "notes": "Official release. Improvements & Bugfixes.",
+    "download": "https://keymaster.fivem.net/asset-grants",
+    "changelog": "https://discord.gg/soon"
+}
